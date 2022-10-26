edc6a33609501f9f79acd01b4755fe389b384ab5
Masyukov Pavel <Developer-RU@users.noreply.github.com>
2022-10-26 19:18:25 +0500

    Edit config and main

--- include/app_config.hpp
+++ include/app_config.hpp
@@ -5,10 +5,18 @@
 #include "Arduino.h"
 
 
+
+#define BAUNDRATE       115200
+
 #define PIN_RELE        4
 
 #define POS_GERCON      2
 #define NEG_GERCON      3
+
+
+
+#define TIME_PRESS      1500
+#define TIME_PROC       5000
 
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include "main.hpp"
 
+
+unsigned long timerTest = 0;
+
+bool flagStart = false;
+bool flarPos = false, flarNeg = false;
+
+
 /**
  * @brief 
  * 
  */
 void setup() 
 {
-    pinMode(POS_GERCON, INPUT);     digitalWrite(POS_GERCON, LOW);
-    pinMode(NEG_GERCON, INPUT);     digitalWrite(NEG_GERCON, LOW);
+    pinMode(POS_GERCON, INPUT);     digitalWrite(POS_GERCON, HIGH);
+    pinMode(NEG_GERCON, INPUT);     digitalWrite(NEG_GERCON, HIGH);
 
     pinMode(PIN_RELE, OUTPUT);      digitalWrite(PIN_RELE, LOW);
+
+    Serial.begin(BAUNDRATE);
+
+    // Serial.println("OK");
 }
 
 /**
@@ -18,6 +29,70 @@ void setup()
  */
 void loop() 
 {
-    
-    
+
+    if(Serial.available())
+    {
+        delay(20);
+        String message = "";
+        
+        while(Serial.available()) 
+        {
+            message += char(Serial.read());
+            delay(2);
+        }
+
+        Serial.println(message);
+        
+        message.replace(" ","");
+        message.replace("\r","");
+        message.replace("\n","");
+        
+        if (message.indexOf("START") > -1)
+        {
+            // Serial.println("{\"result\":\"OK\"}");
+
+            digitalWrite(PIN_RELE, HIGH);
+
+            timerTest = millis();
+            flagStart = true;
+        }
+    }
+
+    if(flagStart == true && millis() > timerTest + TIME_PRESS)
+    {
+        digitalWrite(PIN_RELE, LOW);
+    }
+
+    if(flagStart == true && millis() < timerTest + TIME_PROC)
+    {
+
+        digitalWrite(PIN_RELE, LOW);
+
+
+        if(digitalRead(POS_GERCON) == LOW)
+        {
+            flarPos = true;
+        }
+
+        if(digitalRead(NEG_GERCON) == LOW)
+        {
+            flarNeg = true;
+        } 
+    }
+
+    if(flagStart == true && millis() >= timerTest + TIME_PROC) 
+    {
+        String res = "";
+
+        res += "{\"result\":";
+        res += flarPos;
+        res += flarNeg;
+        res += "\"}";
+
+        Serial.println(res);
+
+        flagStart = false;
+        flarPos = false;
+        flarNeg = false;
+    } 
 }
