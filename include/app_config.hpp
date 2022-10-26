0db7dba4f622b629de57cacace5a41d1ad7975a6
Masyukov Pavel <Developer-RU@users.noreply.github.com>
2022-10-26 18:52:53 +0500

    DOOR-TESTER

---
+++ include/app_config.hpp
@@ -0,0 +1,14 @@
+#ifndef APP_CONFIG_H_
+#define APP_CONFIG_H_
+
+
+#include "Arduino.h"
+
+
+#define PIN_RELE        5
+
+#define POS_GERCON      2
+#define NEG_GERCON      2
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include "main.hpp"
+
+/**
+ * @brief 
+ * 
+ */
+void setup() 
+{
+    pinMode(POS_GERCON, INPUT);     digitalWrite(POS_GERCON, LOW);
+    pinMode(NEG_GERCON, INPUT);     digitalWrite(NEG_GERCON, LOW);
+
+    pinMode(PIN_RELE, OUTPUT);      digitalWrite(PIN_RELE, LOW);
+}
+
+/**
+ * @brief 
+ * 
+ */
+void loop() 
+{
+    
+}
